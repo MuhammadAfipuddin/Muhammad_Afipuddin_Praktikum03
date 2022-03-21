ede2afdf0cf38e9a190eb94970e3b334ac87a833
muhammad afipuddin <apip398@gmail.com>
2022-03-21 10:27:41 +0700

    pengumpulan tugas praktikum 3

---
+++ atas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SAS-STTNF</title>
+</head>
+<body>
+
+<header role="banner">
+<h2 style="margin-left: 10px">Student Activity Score - STTNF</h2>
+<hr/>
+</header>
+</body>
+</html>
---
+++ bawah.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SAS-STTNF</title>
+    </head>
+    <body>
+        <footer>
+            <p style="margin-left: 10px">Created by <a href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p>
+        </footer>
+    </body> 
+</html>
